946c880889c47005e85ffe2c1ff38ddebbebbfed
Sonyramssen <101753360+Sonyramssen@users.noreply.github.com>
2023-04-20 21:01:21 -0400

    Add files via upload

---
+++ defines.php
@@ -0,0 +1,4 @@
+<?php
+	define( 'INSTAGRAM_APP_ID', '1368580330383685' );
+	define( 'INSTAGRAM_APP_SECRET', '76eaa461cff617b789d5bbf15c8e3aae' );
+	define( 'INSTAGRAM_APP_REDIRECT_URI', 'https://localhost/pythonIG/index.php' );
