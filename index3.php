74a25bdd9399c4cfbff16cb10c4787e81f133949
Sonyramssen <101753360+Sonyramssen@users.noreply.github.com>
2023-04-23 11:50:17 -0400

    Update index3.php

--- index3.php
+++ index3.php
@@ -4,46 +4,54 @@
     <title>Choose a social media platform</title>
     <style>
       body {
-        background-color: #fde6e6;
+        background-color: #f2f2f2;
         font-family: Arial, sans-serif;
         font-size: 16px;
         line-height: 1.5;
+        padding: 20px;
+        margin: 0;
       }
       h2 {
-        color: #ad1f1f;
+        color: #333;
         text-align: center;
+        margin-top: 0;
       }
       ul {
         list-style: none;
         padding: 0;
         margin: 0 auto;
-        max-width: 400px;
+        max-width: 500px;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        flex-wrap: wrap;
       }
       li {
-        margin-bottom: 20px;
+        margin-bottom: 30px;
         text-align: center;
       }
       a {
-        background-color: #f1c40f;
+        background-color: #bfbfbf;
         border: none;
-        border-radius: 3px;
-        color: #fff;
+        border-radius: 4px;
+        color: #333;
         cursor: pointer;
         display: inline-block;
         font-size: 16px;
-        padding: 10px 20px;
+        padding: 12px 24px;
         text-decoration: none;
         transition: background-color 0.3s ease;
+        box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.1);
       }
       a:hover {
-        background-color: #d4af37;
+        background-color: #d9d9d9;
       }
     </style>
   </head>
   <body>
     <h2>Choose a social media platform</h2>
     <ul>
-      <li><a href="https://www.instagram.com">Instagram</a></li>
+      <li><a href="https://localhost/SWEPROJECT/index.php">Instagram</a></li>
       <li><a href="https://www.youtube.com">YouTube</a></li>
       <li><a href="https://www.tiktok.com">TikTok</a></li>
     </ul>
